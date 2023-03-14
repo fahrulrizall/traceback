bd580b60a55022da63305ea4a67ac7b44937143d
fahrulrizall <fahrulrizall@gmail.com>
2023-03-14 15:58:28 +0800

    add logout

--- frontend/src/Pages/MainLayout.jsx
+++ frontend/src/Pages/MainLayout.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header, Sidebar } from "../Components";
 import { Outlet } from "react-router-dom";
 import { ApplicationStoreProvider } from "../Hook/UserHook";
 
 export default function MainLayout() {
+  const [isShowSidebar, setIsShowSidebar] = useState(false);
+
   return (
-    <div>
+    <div className={isShowSidebar ? "toggle-sidebar" : undefined}>
       <ApplicationStoreProvider>
-        <Header />
+        <Header setIsShowSidebar={setIsShowSidebar} />
         <Sidebar />
         <Outlet />
       </ApplicationStoreProvider>
